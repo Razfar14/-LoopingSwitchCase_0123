fe993a32267b7600fccc9401dda5fcbe1cedb379
Razfar14 <rajarangkuty14@gmail.com>
2025-03-18 20:03:44 +0700

    pengualangan_while

---
+++ .vscode/file2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+    int PerualanganWhile;
+    int PerulanganDO;
+
+    srand (time(0));
+    x = rand() % 10;
+    cout << "Prtulangan While " << x << endl;
+    cout << "Nilai x awal = " << x << endl;
+
+    while (x < 7)
+    {
+        cout << "Nilai x awal = " << x << endl;
+        x = rand() % 10;
+        PerualanganWhile++;
+    }
+    cout << "Nilai x terakhir = " << x << endl;
+    cout << "Jumlah perulangan = " << PerualanganWhile << endl;
+
+    cout << endl;
+
+  
